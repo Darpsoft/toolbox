0ee2118994298c56ec2a74927dea3a1ab06b4b62
Pedro Fuentes <pedro@vantageio.com>
2022-03-01 23:13:09 -0400

    refactor: add reducer and persisReducer in navigation file

--- src/navigation/index.tsx
+++ src/navigation/index.tsx
@@ -1,5 +1,8 @@
-import React, { ReactElement } from "react";
+import React, { useEffect } from "react";
+import { View } from "react-native";
 
+// React Native Navigation
+import { SafeAreaProvider } from "react-native-safe-area-context";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { NavigationContainer } from "@react-navigation/native";
 
@@ -9,9 +12,42 @@ import Home from "@screens/DefaultUser/Home";
 // Auth screens
 import Login from "@screens/Auth/Login";
 
+// Redux
+import { RootState } from "@redux/reducers";
+import { useSelector } from "react-redux";
+
+// Utils
+import { storage as database } from "@utils/storage";
+
 const Stack = createNativeStackNavigator();
 
-const MainStack = (): ReactElement => {
+// eslint-disable-next-line prefer-const
+let firstLoad = {
+  auth: true,
+};
+
+const PersistReducer = () => {
+  const auth = useSelector(({ auth }: RootState) => auth);
+
+  useEffect(() => {
+    saveReducerData("auth", auth);
+  }, [auth]);
+  /**
+   * Function to save the data when the Storage is updated,
+   * in this case it will only save <auth>
+   */
+  async function saveReducerData<T>(nameReducer: "auth", storage: T) {
+    if (!firstLoad[nameReducer]) {
+      await database[nameReducer].set(storage, "object");
+    } else {
+      firstLoad[nameReducer] = false;
+    }
+  }
+
+  return <View />;
+};
+
+const MainStack = () => {
   return (
     <Stack.Navigator>
       <Stack.Screen name="Login" component={Login} />
@@ -20,11 +56,14 @@ const MainStack = (): ReactElement => {
   );
 };
 
-const AppCreate = (): ReactElement => {
+const AppCreate = () => {
   return (
-    <NavigationContainer>
-      <MainStack />
-    </NavigationContainer>
+    <SafeAreaProvider>
+      <NavigationContainer>
+        <MainStack />
+        <PersistReducer />
+      </NavigationContainer>
+    </SafeAreaProvider>
   );
 };
 
