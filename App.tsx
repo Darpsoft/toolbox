e6f13a35a157b7a9b17bb34ce9ce8c66eac6cc3b
Pedro Fuentes <pedro@vantageio.com>
2022-03-01 23:37:56 -0400

    refactor: add reducer in App file

--- App.tsx
+++ App.tsx
@@ -1,8 +1,54 @@
 import React from "react";
+import { Store } from "redux";
 import AppCreate from "./src/navigation";
+import reduxStorage from "./src/redux";
+import { IAuthState } from "@redux/reducers/auth";
+import { storage as database } from "./src/utils/storage";
+import _ from "lodash";
+import { useReducerCustom } from "./src/hooks/useReducerCustom";
+import SplashScreen from "./src/components/SplashScreen";
+import { Provider } from "react-redux";
+
+export let storage: Store;
+
+export const getStorageAsync = async (): Promise<{ auth?: IAuthState }> => {
+  const auth = await database.auth.get("object");
+
+  return {
+    ...(_.isEqual(auth, {}) ? {} : { auth }),
+  };
+};
+
+const initialState = {
+  loading: true,
+  reducers: {},
+};
 
 const App = () => {
-  return <AppCreate />;
+  const [state, dispatchComponent] = useReducerCustom(initialState);
+
+  const initialRequest = async () => {
+    (!state.loading || !storage) && dispatchComponent({ loading: true });
+    const reducers = await getStorageAsync();
+    dispatchComponent({ loading: false, reducers });
+  };
+
+  React.useEffect(() => {
+    initialRequest();
+  }, []);
+
+  // This is for add all reducers in variable storage
+  storage = reduxStorage(state.reducers);
+
+  if (state.loading) {
+    return <SplashScreen />;
+  }
+
+  return (
+    <Provider store={storage}>
+      <AppCreate />
+    </Provider>
+  );
 };
 
 export default App;
